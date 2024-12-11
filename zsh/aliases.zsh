61532d0de93d81deaebbe65f34f64372f20e3922
Ronces <matthieu.eschbach@gmail.com>
2024-12-11 12:15:27 +0100

    enable functions and cleaning aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,17 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias tlf="tail -f"
+
+alias mkdir='mkdir -p'
+
+alias l='ls --color=auto'
+alias ll='ls -al'
+alias lh='ls -Alh'
+alias ls="colorls"
+
+alias f=fuck
+
+# Productivity
+alias shame="ack -r "TODO" ."
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,9 +1,9 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
+fpath=(~/.dotfiles/functions $fpath)
 
-autoload -U $ZSH/functions/*(:t)
+autoload -U ~/.dotfiles/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
