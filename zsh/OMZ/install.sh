02112dc8731459c8aad55535336831a5a3730a68
Ronces <matthieu.eschbach@gmail.com>
2025-04-22 18:28:32 +0200

    new install script for OMZ

--- zsh/OMZ/install.sh
+++ zsh/OMZ/install.sh
@@ -2,3 +2,17 @@
 
 echo "install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+plugins="$HOME/.oh-my-zsh/custom/plugins"
+themes="$HOME/.oh-my-zsh/custom/themes"
+
+# PLUGINS
+
+git clone --depth 1 https://github.com/junegunn/fzf.git $plugins/fzf
+git clone https://github.com/jhwohlgemuth/zsh-pentest.git $plugins/zsh-pentest
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $plugins/you-should-use
+git clone https://github.com/fdellwing/zsh-bat.git $plugins/zsh-bat
+
+# THEMES
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $themes/powerlevel10k
