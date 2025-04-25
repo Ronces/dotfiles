54fe3004036e214c27493c6ca5a4e39db396c327
Ronces <matthieu.eschbach@gmail.com>
2025-04-25 13:06:52 +0200

    fix tmux config

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,11 +2,12 @@
 
 echo "› Install tmux"
 sudo apt install tmux
-echo ""
 
-#if [[-d ~/.tmux]]; then
-#  rm -rf ~/.tmux
-#fi
-#
-# git clone https://github.com/gpakosz/.tmux.git ~/.tmux
-# ln -sf ~/.tmux/.tmux.conf ~/.dotfiles/tmux/tmux.conf.symlink
+if [ -d ~/.tmux ]; then
+  rm -rf ~/.tmux
+fi
+
+ git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+ ln -sf ~/.tmux/.tmux.conf ~/.dotfiles/tmux/tmux.conf.symlink
+
+echo ""
