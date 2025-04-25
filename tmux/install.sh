a0d50878fea9a3b9232f4b37a17912e5f28a249c
Ronces <matthieu.eschbach@gmail.com>
2025-04-25 07:15:52 +0200

    simpler tmux install

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,9 +2,9 @@
 
 sudo apt install tmux
 
-if [[-d ~/.tmux]]; then
-  rm -rf ~/.tmux
-fi
-
- git clone https://github.com/gpakosz/.tmux.git ~/.tmux
- ln -sf ~/.tmux/.tmux.conf ~/.dotfiles/tmux/tmux.conf.symlink
+#if [[-d ~/.tmux]]; then
+#  rm -rf ~/.tmux
+#fi
+#
+# git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+# ln -sf ~/.tmux/.tmux.conf ~/.dotfiles/tmux/tmux.conf.symlink
