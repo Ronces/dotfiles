344f99cb639880234ca855b5035debb17e245e67
Ronces <matthieu.eschbach@gmail.com>
2025-04-27 22:42:21 +0200

    vim/install.sh now install your plugins

--- vim/install.sh
+++ vim/install.sh
@@ -8,3 +8,21 @@ if ! [ -d ~/.vim/undo ]; then
 fi
 
 echo ""
+
+echo "› Install vim-plug"
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+  echo "Vim-plug is already installed"
+fi
+
+echo ""
+
+echo "› Install vim plugins"
+vim +'PlugUpgrade --sync' +qa
+echo "Plug up to date"
+vim +'PlugInstall --sync' +qa
+echo "Plugins installed"
+vim +'PlugUpdate --sync' +qa
+echo "Plugins up to date"
+
